e3fa26f698166b594c82cf84fd2d6f5be47e3d5b
Ashish yadav <ashish.yadav2021@gmail.com>
2023-02-27 10:10:20 +0530

    complete

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import DirectChat from './DirectChat';
+import './App.css';
+
+//Using arrow function component
+const App = () => {
+  return (
+    // <ChatEngine
+    //   userName='ashish'
+    //   projectID = 'f51aabe0-ccbf-4f53-b148-19a4265d99b7'
+    //   userSecret='93ab8ff3-718a-439a-9c18-5a6df98053e7'
+
+    // />,
+    <DirectChat/>
+  )
+}
+
+export default App
+
+
